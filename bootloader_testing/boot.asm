13ae1c35049fae24f9560369bdde62ba8f92e019
Henry Baldwin <henrymbaldwin@gmail.com>
2022-12-20 02:01:51 -0600

    hello (world is implied) boot

--- bootloader_testing/boot.asm
+++ bootloader_testing/boot.asm
@@ -1,5 +1,19 @@
-loop:
-	jmp loop
+
+
+
+mov ah, 0x0e ;set tty mode for 0x10 intterupt
+mov al, 'H'
+int 0x10 ;bios intterupt, basically says "print al"
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $ ;jump to current address, infinite loop
 
 times 510-($-$$) db 0
 
